7537ecca8f4ca9c988a1c3e381d5e4ed1f0f7591
pnavab <114110926+pnavab@users.noreply.github.com>
2024-02-17 18:36:13 -0800

    edit home page

--- TruckrZzz/TruckrZzz.py
+++ TruckrZzz/TruckrZzz.py
@@ -9,7 +9,6 @@ from TruckrZzz.components.footer import footer
 from TruckrZzz.webcam import *
 from fastapi import FastAPI, Request
 import reflex as rx
-from TruckrZzz.tester import *
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -24,60 +23,64 @@ def index() -> rx.Component:
         navbar(),
         rx.vstack(
             rx.box(
-                rx.heading("Saving Lives One Heartbeat at a Time"),
+                rx.heading("Saving Lives One Heartbeat at a Time", size="8"),
                 padding="5px",
-                margin_top="5px"
+                margin_top="5px",
             ),
             rx.box(
                 rx.image(src="/truck2.gif",width="100vw",height="40vh"),
                 border_radius="15px",
-                # width="fill",
-                # height="fill"
             ),  
             rx.scroll_area(
                 rx.hstack(
-                    rx.box(
+                    rx.vstack(
                         rx.box(
                             rx.image(src="/webcam.png",
-                                     width="100%", height="100%"),
-                            width="8em",
-                            height="25%"
+                                     width="100", height="100%", object_fit="cover"),
+                            width="100%",
+                            height="35%"
                         ),
-                        rx.heading("Webcam Integration",
-                                   text_align="center", size="5", padding="5px"),
+                        rx.spacer(),
+                        rx.heading("Webcam Integration",text_align="center", padding="5px", margin_top="15px", size="8"),
                         rx.fragment(
                             rx.box(
-                                rx.text("Our project incorporates facial recognition technology leveraging the webcam to monitor the sleepiness level of truck drivers in real time. By analyzing facial cues and patterns, the system assesses signs of drowsiness, alerting the driver and playing a warning sound to wake the driver up. This innovative feature not only prioritizes the well-being of drivers but also enhances overall fleet management efficiency.", size="2", text_align="center"),
+                                rx.text("Our project incorporates facial recognition and landmarking technology leveraging the webcam to monitor the sleepiness level of truck drivers in real time. By analyzing facial cues and patterns, the system assesses signs of drowsiness, alerting the driver and playing a warning sound to wake the driver up. This innovative feature not only prioritizes the well-being of drivers but also enhances overall fleet management efficiency.", size="2", text_align="center"),
                                 margin_bottom="5px",
                                 margin_top="5px",
-                                margin_left="5px",
-                                margin_right="5px"
+                                margin_left="10px",
+                                margin_right="10px",
+                                size="8"
                             ),
                         ),
+                        rx.spacer(),
                         bg="white",
+                        align_items="center",
                         width="27vw",
                         height="60vh",
                         margin_bottom="20px",
                         margin_top="20px",
                         margin_right="20px",
                         margin_left="20px",
-
+                        border_radius="8px",
+                        overflow_x="wrap",
+                        overflow_y="scroll"
                     ),
                     rx.spacer(),
                     rx.vstack(
-                        rx.heading("Livestream Heartrate",text_align="center",size="5",padding="5px"),
+                        rx.heading("Livestream Heartrate",text_align="center",padding="5px", margin_top="15px", size="8"),
                         rx.fragment(
                             rx.box(
                                 rx.text("Our project integrates a real-time heart rate monitor to continuously track the heart rate of truck drivers during their journeys. By leveraging this data, our system provides insight into the driver's stress levels and  fatigue. With our advanced heart rate monitoring feature, we prioritize the health and performance of truck drivers, contributing to enhanced fleet management and overall operational excellence.", size="2", text_align="center"),
                                 margin_bottom="5px",
                                 margin_top="5px",
-                                margin_left="5px",
-                                margin_right="5px"
+                                margin_left="10px",
+                                margin_right="10px",
+                                size="7"
                             ),
                         ),
                         rx.spacer(),
                         rx.box(
-                            rx.image(src="/webcam.png",width="100%",height="100%"),
+                            rx.image(src="/heart-rate-banner.png",width="100%",height="100%", margin_top="5px", margin_right="5px", margin_left="5px"),
                             width="100%",
                             height="40%",
                             bottom="0"
@@ -90,46 +93,48 @@ def index() -> rx.Component:
                         margin_right="20px",
                         margin_left="20px",
                         align_items="center",
+                        border_radius="8px",
                         _hover={"transform": "rotate(0)"}
                     ),
                     rx.spacer(),
-                    rx.box(
+                    rx.vstack(
                         rx.box(
                             rx.image(src="/webcam.png",
-                                     width="100%", height="100%"),
-                            width="8em",
+                                     width="100%", height="100%", margin_top="5px", margin_right="5px", margin_left="5px"),
+                            width="100%",
                             height="25%"
                         ),
-                        rx.heading("Additional Feature",
-                                   text_align="center", size="5", padding="5px"),
+                        rx.spacer(),
+                        rx.heading("Another Good Point",text_align="center", padding="5px", margin_top="15px", size="8"),
                         rx.fragment(
                             rx.box(
-                                rx.text(
-                                    "This is some sample text for card 1. This will be where the description of our webcam integration goes. Here are some extra sentences.", size="2", text_align="center"),
+                                rx.text("this is some sample text for another card we will have on here.", size="2", text_align="center"),
                                 margin_bottom="5px",
                                 margin_top="5px",
-                                margin_left="5px",
-                                margin_right="5px"
+                                margin_left="10px",
+                                margin_right="10px",
+                                size="7"
                             ),
                         ),
                         bg="white",
+                        align_items="center",
                         width="27vw",
                         height="60vh",
                         margin_bottom="20px",
                         margin_top="20px",
                         margin_right="20px",
                         margin_left="20px",
+
                     ),
                     rx.spacer(),
                     rx.box(
                         rx.box(
                             rx.image(src="/webcam.png",
-                                     width="100%", height="100%"),
+                                     width="100%", height="100%", margin_top="5px", margin_right="5px", margin_left="5px"),
                             width="8em",
                             height="25%"
                         ),
-                        rx.heading("Additional Feature",
-                                   text_align="center", size="5", padding="5px"),
+                        rx.heading("Another Good Point",text_align="center", padding="5px", margin_top="15px", size="8"),
                         rx.fragment(
                             rx.box(
                                 rx.text(
@@ -137,10 +142,12 @@ def index() -> rx.Component:
                                 margin_bottom="5px",
                                 margin_top="5px",
                                 margin_left="5px",
-                                margin_right="5px"
+                                margin_right="5px",
+                                size="7"
                             ),
                         ),
                         bg="white",
+                        align_items="center",
                         width="27vw",
                         height="60vh",
                         margin_bottom="20px",
--- TruckrZzz/components/navbar.py
+++ TruckrZzz/components/navbar.py
@@ -19,10 +19,10 @@ class Navbar(rx.Component):
         return rx.hstack(
             # The logo with larger text on the left.
             rx.link(
-                rx.heading("TruckrZzz", size="7"),
+                rx.heading("TruckrZzz", size="9", color="#1d2d44"),
                 href="/",
+                transition="transform 0.2s ease-in-out",
                 _hover={
-                    "transition": "transform 0.2s ease-in-out",
                     "text_decor": "none",
                     "transform": "scale(1.1)",
                 }
@@ -30,13 +30,13 @@ class Navbar(rx.Component):
             rx.spacer(),
             # Centered text links "home", "dash", "graph".
             rx.center(
-                rx.link("Home", href="/", padding="1em", border_radius="0.5em", background_color=rx.cond(self.is_selected(""), "#ADD8E6", "transparent"), text_decoration=rx.cond(self.is_selected(""), "underline", "none"), _hover={"text_decoration": "underline"}),
+                rx.link("Home", href="/", padding="1em", border_radius="0.5em", color=rx.cond(self.is_selected(""), "#ffffff", "black"), background_color=rx.cond(self.is_selected(""), "#3e5c76", "transparent"), text_decoration=rx.cond(self.is_selected(""), "underline", "none"), _hover={"text_decoration": "underline"}),
                 rx.spacer(width="20px"),
-                rx.link("Dashboard", href="/dashboard", padding="1em", border_radius="0.5em", background_color=rx.cond(self.is_selected("dashboard"), "#ADD8E6", "transparent"), text_decoration=rx.cond(self.is_selected("dashboard"), "underline", "none"), _hover={"text_decoration": "underline"}),
+                rx.link("Dashboard", href="/dashboard", padding="1em", border_radius="0.5em", color=rx.cond(self.is_selected("dashboard"), "#ffffff", "black"), background_color=rx.cond(self.is_selected("dashboard"), "#3e5c76", "transparent"), text_decoration=rx.cond(self.is_selected("dashboard"), "underline", "none"), _hover={"text_decoration": "underline"}),
                 rx.spacer(width="20px"),
-                rx.link("Graph", href="/graphs/6", padding="1em", border_radius="0.5em", background_color=rx.cond(self.is_selected("graphs"), "#ADD8E6", "transparent"), text_decoration=rx.cond(self.is_selected("graphs"), "underline", "none"), _hover={"text_decoration": "underline"}),
+                rx.link("Graph", href="/graphs/6", padding="1em", border_radius="0.5em", color=rx.cond(self.is_selected("graphs"), "#ffffff", "black"), background_color=rx.cond(self.is_selected("graphs"), "#3e5c76", "transparent"), text_decoration=rx.cond(self.is_selected("graphs"), "underline", "none"), _hover={"text_decoration": "underline"}),
                 rx.spacer(width="20px"),
-                rx.link("Webcam", href="/webcam", padding="1em", border_radius="0.5em", background_color=rx.cond(self.is_selected("webcam"), "#ADD8E6", "transparent"), text_decoration=rx.cond(self.is_selected("webcam"), "underline", "none"), _hover={"text_decoration": "underline"}),
+                rx.link("Webcam", href="/webcam", padding="1em", border_radius="0.5em", color=rx.cond(self.is_selected("webcam"), "#ffffff", "black"), background_color=rx.cond(self.is_selected("webcam"), "#3e5c76", "transparent"), text_decoration=rx.cond(self.is_selected("webcam"), "underline", "none"), _hover={"text_decoration": "underline"}),
                 spacing="4"
             ),
             rx.spacer(),
@@ -57,7 +57,10 @@ class Navbar(rx.Component):
             ),
             width="100%",
             padding="1em",
-            border_bottom=styles.border
+            background_color="#f0ebd8",
+            height="15%",
+            border_radius="0 0 10px 10px",
+            border_bottom=styles.border,
         )
 
 def navbar() -> rx.Component:
